b559b648150a45fea196a433ee10c4c6e021888c
Heitor Monteiro Padovese <167241925+Heitormonteiroo@users.noreply.github.com>
2025-03-24 11:04:45 -0400

    Add files via upload

---
+++ aula01.cpp
@@ -0,0 +1,137 @@
+#include <cstdio>
+
+class Celula;
+class TabelaDispersao;
+
+class Celula
+{
+  friend TabelaDispersao;
+  
+public:
+  Celula(int chave, Celula *prox = nullptr) : chave(chave), prox(prox) { }
+  
+private:
+  int chave;
+  Celula *prox;
+};
+
+class TabelaDispersao
+{
+public:
+  TabelaDispersao(int tamanho);
+  ~TabelaDispersao();
+  void insere(int x);
+  bool busca(int x);
+  bool remove(int x);
+  double fator_carga();
+  int elementos(); // devolve a quantidade de elementos
+  void escreve();
+  
+private:
+  int h(int x);
+  int m;
+  Celula **tabela;
+};
+
+int main(void)
+{
+    TabelaDispersao tabela(5); // Tabela com 5 buckets
+
+  // Inserindo elementos
+  tabela.insere(1);
+  tabela.insere(6);  // Vai colidir com 1 (1 % 5 == 1, 6 % 5 == 1)
+  tabela.insere(11); // Também colide na posição 1
+  tabela.insere(3);
+
+  printf("Tabela após inserções:\n");
+  tabela.escreve();
+  
+bool teste=tabela.busca(10);
+if(teste==true){
+    printf("achou");
+}else{
+    printf("Nao achou");
+}
+int teste2=tabela.elementos();
+printf("%d",teste2);
+  
+  /* Teste de remoção
+  printf("Removendo 6...\n");
+  tabela.remove(6);
+  tabela.escreve();*/
+
+  /*printf("Removendo 10 (não existe)...\n");
+  tabela.remove(10);
+  tabela.escreve();*/
+
+  // Teste de fator de carga
+  // printf("Fator de carga: %.2f\n", tabela.fator_carga());
+
+
+  return 0;
+}
+
+TabelaDispersao::TabelaDispersao(int tamanho) :
+  m(tamanho)
+{
+  tabela = new Celula*[tamanho];
+  for (int i = 0; i < tamanho; i++)
+    tabela[i] = nullptr;
+}
+
+TabelaDispersao::~TabelaDispersao()
+{
+  // FAZER: varrer cada lista deletando as células
+  delete[] tabela;
+}
+
+void TabelaDispersao::insere(int x)
+{
+  Celula *nova = new Celula(x, tabela[h(x)]);
+  tabela[h(x)] = nova;
+}
+
+bool TabelaDispersao::busca(int x)
+{
+  Celula *aux;
+  for(aux=tabela[h(x)];aux!=nullptr && aux->chave!=x;aux=aux->prox);
+  if (aux!=nullptr){
+    return true;}else{
+    return false;}
+}
+
+bool TabelaDispersao::remove(int x)
+{
+  // FAZER: varrer a lista em tabela[h(x)], removendo a célula com x se encontrar e devolvendo true, ou devolvendo false se não encontrar
+  return true;
+}
+
+double TabelaDispersao::fator_carga()
+{
+  return elementos() / (double) m;
+}
+
+int TabelaDispersao::elementos()
+{
+    Celula *aux;
+    int c=0;
+    for(int i=0;i<m;i++){
+        for(aux=tabela[i];aux!=nullptr;aux=aux->prox){
+            c+=1;
+        }
+    }
+  return c;
+}
+
+int TabelaDispersao::h(int x)
+{
+  return x % m;
+}
+void TabelaDispersao :: escreve(){
+    Celula *aux;
+    for(int i=0;i<m;i++){
+        for(aux=tabela[i];aux!=nullptr;aux=aux->prox){
+            printf("%d ",aux->chave);
+        }
+    }
+}
